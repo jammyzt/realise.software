6da16b821b0051fefbc621c99e15c5f21b2f372b
James Cameron Taylor <jctaylor92@outlook.com>
2021-12-07 18:42:27 +0000

    🎨 Add logos

---
+++ realise.software/Pages/Logos.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace realise.software.Pages;
+
+public class LogosModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public LogosModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
+
